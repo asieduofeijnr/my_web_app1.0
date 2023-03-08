25fed0b6ea81eb076b614b8290d010e4c585e6ff
asieduofeijnr <114332208+asieduofeijnr@users.noreply.github.com>
2023-03-08 09:06:39 +0000

    my_first_commit

---
+++ functions.py
@@ -0,0 +1,24 @@
+
+# This function reads a list of to-do items from a file and returns it as a list.
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+# This function writes a list of to-do items to a file.
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do items list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print("hello")
+    print(get_todos())
